3da85898c2936c23b65ee315b14cc90ab606d913
dreyso <andreysteb@gmail.com>
2024-08-16 17:28:39 -0700

    fix flashing menu on page load

--- nav.js
+++ nav.js
@@ -1,40 +1,39 @@
-window.addEventListener('load', () => {
-
+window.addEventListener("load", () => {
   const menu = document.querySelector("#nav-menu");
   let menuWidth = menu.offsetWidth;
-  const navbar = document.querySelector('#banner');
+  const navbar = document.querySelector("#banner");
   let navbarHeight = navbar.offsetHeight;
-  
+
   // Resize safety
-  window.addEventListener('resize', () => {
+  window.addEventListener("resize", () => {
     menuWidth = menu.offsetWidth;
     navbarHeight = navbar.offsetHeight;
   });
-  
+
   // Set menu initial position
   let menuVisible = false;
   const menuToggle = document.querySelector("#mobile-menu-toggle");
-  
+
   const setMenuPos = () => {
     menu.style.right = menuVisible ? `0` : `-${menuWidth}px`;
-    menu.style.top = `${navbarHeight}px`;;
+    menu.style.top = `${navbarHeight}px`;
   };
-  
+
   const icon = document.querySelector("#mobile-menu-icon");
 
-  menuToggle.addEventListener('click', () => {
+  menuToggle.addEventListener("click", () => {
     menuVisible = !menuVisible;
     setMenuPos();
-    
+
     icon.classList.toggle("menu-button-clicked");
 
-    icon.src = icon.src.endsWith('img/menu-closed.svg') 
-        ? 'img/menu-open.svg' 
-        : 'img/menu-closed.svg';
+    icon.src = icon.src.endsWith("img/menu-closed.svg")
+      ? "img/menu-open.svg"
+      : "img/menu-closed.svg";
   });
 
   setMenuPos(); // On page load
+  setTimeout(() => {
+    menu.style.visibility = "visible";
+  }, 500); // Fix flashing menu on page load
 });
-
-    
-    
