e677c5c0f52e34e050b19faed565c847833bbfb0
dreyso <andreysteb@gmail.com>
2024-10-06 18:08:30 -0700

    animated abode section

--- src/sections/abode.js
+++ src/sections/abode.js
@@ -1,21 +1,46 @@
 import headshot from "../assets/imgs/abode.webp"
+import { motion } from 'framer-motion';
+
+const headerVariants = {
+  hidden: { opacity: 0, y: -100 }, 
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 1, ease: "easeInOut" } 
+  },
+};
+
+const imgVariants = {
+  hidden: { opacity: 0, y: 50 },
+  visible: {
+    opacity: 1,
+    y: 0, 
+    transition: { duration: 1, ease: "easeInOut" }
+  },
+};
 
 function Abode() {
   return (
     <header id="abode" className="main-section">
       <div className="upper-wrapper centered">
-        <h1>
-          Hi, <br />
-          I'm
-          <span className="highlight">Andrey</span>
-        </h1>
-        <img
-          src={headshot}
-          width="1812"
-          height="2500"
-          className="fade-down"
-          alt="Authors headshot"
-        />
+      <motion.h1
+        initial="hidden"
+        animate="visible"
+        variants={headerVariants}
+      >
+        Hi, <br />
+        I'm <span className="highlight">Andrey</span>
+      </motion.h1>
+      <motion.img
+        src={headshot}
+        width="1812"
+        height="2500"
+        className="fade-down"
+        alt="Author's headshot"
+        initial="hidden"
+        animate="visible"
+        variants={imgVariants}
+      />
       </div>
       <div className="lower-wrapper centered">
         <p>
