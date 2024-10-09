01074eabb14a7425852940591f32cdf6717791e7
dreyso <andreysteb@gmail.com>
2024-10-08 20:31:16 -0700

    add react to skills

--- src/sections/details.js
+++ src/sections/details.js
@@ -34,8 +34,7 @@ function Details() {
         <li>PSU | 2024 | summa cum laude</li>
         <li>Bachelors in Computer Science</li>
         <li>C | C++ | Python</li>
-        <li>Web dev (CSS Grid | Flexbox | JS)</li>
-        <li>Flask | SQL</li>
+        <li>Web dev (React | Fask | SQL)</li>
         <li>Visual Studio | VS Code | Vim</li>
         <li>Git</li>
         <li>Native Russian speaker</li>
